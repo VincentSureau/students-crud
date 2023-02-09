dd54753b8324e98bd0dafc66f45dd397f48c46a4
Vincent Sureau <vincentsureau5@gmail.com>
2023-02-09 16:23:57 +0100

    student creation form integration

---
+++ create.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ajouter un élève</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+  <body>
+    <main class="container p-5">
+        <h1>Ajouter un élève</h1>
+        <form method="POST">
+            <div class="row">
+                <div class="col">
+                    <label for="lastname">Nom</label>
+                    <input type="text" class="form-control" placeholder="Nom" name="lastname">
+                </div>
+                <div class="col">
+                    <label for="firstname">Prénom</label>
+                    <input type="text" class="form-control" placeholder="Prénom" name="firstname">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="gender">Genre</label>
+                <select class="form-control" id="gender" name="gender">
+                    <option value="homme">Homme</option>
+                    <option value="femme">Femme</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="birthdate">Date de naissance</label>
+                <input type="date" class="form-control" id="birthdate" placeholder="Date de naissance" name="birthdate">
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" id="email" placeholder="Email" name="email">
+            </div>
+            <div class="form-group mt-2">
+                <button type="submit" class="btn btn-primary w-100">Ajouter</button>
+            </div>
+        </form>
+        <a href="index.php" class="btn btn-link mt-3">retour</a>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -25,7 +25,7 @@ function format_birthdate($date)
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Ma liste d'élève</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
@@ -33,7 +33,7 @@ function format_birthdate($date)
     <main class="container p-5">
         <h1>Ma liste d'élèves</h1>
         <div class="d-flex justify-content-end">
-            <a href="" class="btn btn-primary"><i class="fa-regular fa-plus"></i> Ajouter un élève</a>
+            <a href="create.php" class="btn btn-primary"><i class="fa-regular fa-plus"></i> Ajouter un élève</a>
         </div>
         <table class="table table-striped table-hover">
             <thead>
